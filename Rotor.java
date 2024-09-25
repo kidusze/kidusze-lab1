8e00d8105b83cd97545af5be145fd075d7b68dde
Kidus Zerihun <kiduszerihun@Kiduss-MacBook-Pro.local>
2024-09-25 16:23:21 -0400

    Lab commit

--- Rotor.java
+++ Rotor.java
@@ -14,16 +14,29 @@ public class Rotor {
     
     public boolean rotate(){
         //TODO
+        // rotorValues = rotorValues.substring(1) + rotorValues.charAt(0);
+
+        // Check if the rotors current character matches the starting character
+        if(rotorValues.charAt(0) == startChar) {
+            return true;
+        }
+        else {
+            return false;
+        }
                
     }
     
 
     public int indexOf(char c){
         //TODO
+        // Find the index of a given character in rotor strings
+        return rotorValues.indexOf(c);
     }
 
     public char charAt(int idx){
         //TODO
+        // We return the chracter at the given index
+        return rotorValues.charAt(idx);
     }
 }
     
